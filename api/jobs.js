f32da17a134f8006ea0e196cc437766dccc5980b
citynet-rivco <andrew.sayegh@citynet.org>
2026-03-11 10:19:43 -0700

    Update jobs.js

--- api/jobs.js
+++ api/jobs.js
@@ -1,1 +1,30 @@
+export default async function handler(req, res) {
+  try {
 
+    const API_KEY = process.env.SERPAPI_KEY;
+
+    if (!API_KEY) {
+      return res.status(500).json({
+        error: "Missing SERPAPI_KEY environment variable"
+      });
+    }
+
+    const url =
+      `https://serpapi.com/search.json?engine=google_jobs&q=jobs+Murrieta+CA&api_key=${API_KEY}`;
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+
+    return res.status(200).json(data);
+
+  } catch (error) {
+
+    console.error("API ERROR:", error);
+
+    return res.status(500).json({
+      error: "Serverless function failed"
+    });
+
+  }
+}
